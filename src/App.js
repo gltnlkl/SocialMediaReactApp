8861c108ac32da0a6f667af72a3afeffdd6c309d
Gulten Ulukal <gulten.ulukal@gmail.com>
2022-02-24 12:07:40 +0300

    added redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import './App.css';
 import NewsFeed from './pages/NewsFeed';
 function App() {
+
+  // STORE OLUSTURULACAK
+
+
+  // ACTION 
+
+
+  // REDUCER
+
+
+  // DISPATCH  (TETIKLEMEK ICIN)
+
+
+
+
+
   return (
     <NewsFeed />
   );
